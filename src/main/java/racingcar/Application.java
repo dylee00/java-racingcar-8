a952e528121fc365f2a22b34408c78ce6eec5e92
dylee00 <dylee6820@gmail.com>
2025-10-27 01:29:36 +0900

    feat: 경주할 자동차 이름 입력

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,20 @@
 package racingcar;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        //자동차의 이름 입력
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String input = scanner.nextLine();
+
+        List<String> carNames = Arrays.asList(input.split(","));
+
+
     }
 }
