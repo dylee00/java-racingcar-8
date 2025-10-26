fd133803e5a6b429ddb2cb484b8b5ff4ec481943
dylee00 <dylee6820@gmail.com>
2025-10-27 01:34:46 +0900

    feat: 자동차 경주 시도 횟수 입력

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -8,12 +8,16 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
-        //자동차의 이름 입력
         Scanner scanner = new Scanner(System.in);
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String input = scanner.nextLine();
 
         List<String> carNames = Arrays.asList(input.split(","));
+
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        Integer count = scanner.nextInt();
+        scanner.close();
+
 
 
     }
